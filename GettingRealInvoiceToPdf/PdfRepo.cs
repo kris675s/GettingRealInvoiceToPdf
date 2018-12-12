f393c494b147829e655548d66c077eee449a0ad1
kris675s <kris675s@eul.eal.dk>
2018-12-12 12:42:56 +0100

    test

---
+++ GettingRealInvoiceToPdf/PdfRepo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+using PdfSharp;
+using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+using System.Collections;
+
+namespace GettingRealInvoiceToPdf
+{
+    public class PdfRepo
+    {
+        public void SortedList SortedPdf(Id.Pdf)
+        {
+
+        }
+        document.Save(filename);
+        Process.Start(filename);
+    }
+}
