51c4d2b5ffc597a69e7d3f05a92cb1cf694a5f1b
kris675s <kris675s@eul.eal.dk>
2018-12-12 14:54:00 +0100

    commit

--- GettingRealInvoiceToPdf/PdfRepo.cs
+++ GettingRealInvoiceToPdf/PdfRepo.cs
@@ -15,11 +15,18 @@ namespace GettingRealInvoiceToPdf
 {
     public class PdfRepo
     {
-        public void SortedList SortedPdf(Id.Pdf)
-        {
+        private static DateTime date = new DateTime();
 
+        public void SortedPdf(Id.Pdf)
+        {
+            public static string FilePath { get; set; }
+            private static string filePath = (@"C:\Program Files\" + @"InvoiceToPdf\" + date.Year + @"\" + date.Month + @"\" + date.Day);;
+        //if user sets path then dont run filepath
+        System.IO.FileInfo PdfInvoice = new System.IO.FileInfo(FilePath);
+            //
+            PdfInvoice.Directory.Create(); // If the directory already exists, this method does nothing.
         }
-        document.Save(filename);
+        document.Save(filename.path());
         Process.Start(filename);
     }
 }
