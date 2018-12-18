11342fc13e648181009bf30f2f61984165841fa4
kris675s <kris675s@eul.eal.dk>
2018-12-18 11:56:21 +0100

    mail eksempler

--- GettingRealInvoiceToPdf/EmailProcessing.cs
+++ GettingRealInvoiceToPdf/EmailProcessing.cs
@@ -9,8 +9,7 @@ namespace GettingRealInvoiceToPdf
 {
     public class EmailProcessing
     { 
-       * / 
-            stack overflow attaching a dynamically pdf to a email c# . net
+           // stack overflow attaching a dynamically pdf to a email c# asp.net
         <system.net>
     <mailSettings>
       <smtp>
@@ -18,9 +17,11 @@ namespace GettingRealInvoiceToPdf
       </smtp>
     </mailSettings>
 
-        
+
+       /*
         SmtpClient smtpClient = new SmtpClient(WebMail.SmtpServer);
         MailMessage email1 = new MailMessage();
+
         email1.IsBodyHtml = true;
             email1.From = new MailAddress("from@email.com");
         email1.To.Add(new MailAddress(sendemail));
@@ -34,5 +35,32 @@ namespace GettingRealInvoiceToPdf
 
         return message("all mails have been sent and the list is empty")
      */
-    }
-}
+       //andet eksempel
+       /*
+            private void SendIt()
+      {
+          var fromAddress = "";
+          var toAddress = "";
+          //Password of your gmail address
+          const string fromPassword = "";
+          // smtp settings
+          var smtp = new System.Net.Mail.SmtpClient();
+          {
+              smtp.Host = "";
+              smtp.Port = 25;
+              smtp.EnableSsl = false;
+              smtp.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network;
+              smtp.Credentials = new NetworkCredential(fromAddress, fromPassword);
+              smtp.Timeout = 20000;
+          }
+          MailMessage msg = new MailMessage();
+          MailAddress ma = new MailAddress(fromAddress);
+          msg.To.Add(toAddress);
+          msg.From = ma;
+          msg.Attachments.Add(new Attachment(@"C:\temp\myreport.log"));
+          msg.Body = "Your body message";
+          msg.Subject = "Your subject line";
+          smtp.Send(msg);
+      }*/
+      }
+  }
